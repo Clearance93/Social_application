2fa0b5abc5ea3c6bc1e9ed1a2a961c5c7f102b98
Clearance Morumudi <clearancemorumudi@outlook.com>
2021-07-16 11:46:25 +0200

    second commit

--- model/chat.js
+++ model/chat.js
@@ -5,7 +5,9 @@ var Schema = mongoose.Schema;
 var chatSchema = new Schema({
   name: String,
   chat: String,
-})
+  
+
+}, { timestamp: true });
 
 
 const Chat = mongoose.model('chat', chatSchema);
